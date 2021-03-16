fb7f8ab03899a77b39edbe75e5249d8a3516d106
pgk <pgk977@outlook.com>
2021-03-16 19:30:17 +0100

    zarovizsga

--- src/test/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypesTest.java
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypesTest.java
@@ -1,4 +1,4 @@
-/*package dogtypes;
+/*package hu.nive.ujratervezes.zarovizsga.dogtypes;
 
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
--- src/test/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHoursTest.java
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHoursTest.java
@@ -1,4 +1,4 @@
-/*package workhours;
+/*package hu.nive.ujratervezes.zarovizsga.workhours;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +9,7 @@ class WorkHoursTest {
     @Test
     void minWork() {
         WorkHours workHours = new WorkHours();
-        String result = workHours.minWork("src/test/resources/hu/nive/ujratervezes/zarovizsga/workhours/workhours.txt");
+        String result = workHours.minWork("src/test/resources/hu/nive/ujratervezes/zarovizsga/hu.nive.ujratervezes.zarovizsga.workhours/hu.nive.ujratervezes.zarovizsga.workhours.txt");
         assertEquals("John Doe: 2021-01-04", result);
     }
 }
