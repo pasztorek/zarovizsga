e64fd9cad5dd497b2edda8c399edb8d9e7849438
pgk <pgk977@outlook.com>
2021-03-16 17:48:44 +0100

    zarovizsga

---
+++ src/test/java/workhours/WorkHoursTest.java
@@ -0,0 +1,17 @@
+/*package workhours;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkHoursTest {
+
+    @Test
+    void minWork() {
+        WorkHours workHours = new WorkHours();
+        String result = workHours.minWork("src/test/resources/hu/nive/ujratervezes/zarovizsga/workhours/workhours.txt");
+        assertEquals("John Doe: 2021-01-04", result);
+    }
+}
+
+ */
